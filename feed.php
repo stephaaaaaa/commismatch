7056db792478c2092e59b1a84c83b78df2425665
Stephanie Labastida <stephanielabastida@u.boisestate.edu>
2018-11-07 09:35:09 -0700

    testing deploy

--- feed.php
+++ feed.php
@@ -11,7 +11,7 @@
 	$dao = new Dao();
 ?>
 
-<!DOCTYPE html>
+<!-- <!DOCTYPE html> -->
 
 <!-- Bootstrap and other scripts -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -23,34 +23,34 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 <!-- Initializer stuff -->
-<html lang="en">
-<head>
-  <title>Commismatch: Feed</title>
+<!-- <html lang="en"> -->
+<!-- <head> -->
+  <!-- <title>Commismatch: Feed</title>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
+  <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+<!-- </head> -->
 <!-- End initializer stuff -->
 
-<body>
-	<?php var_dump($_SESSION);?>
+<!-- <body> -->
 
-	<div class="col-lg-12">
-		<div id="body" class="welcome col-lg-12">
-			<h1>Welcome, <?= $_SESSION['currentUser']['handle']?>!</h1>
-			<!-- Use actual followers + following -->
-			<h5></h5>
+		<div class="col-lg-12">
+			<div id="body" class="welcome col-lg-12">
+				<h1>Welcome, <?= $_SESSION['currentUser']['handle']?>!</h1>
+				<!-- Use actual followers + following -->
+				<h5></h5>
 
-			<h2>Popular Artists</h2>
-			
-			<h2>Popular Commissions</h2>
+				<h2>Popular Artists</h2>
+				
+				<h2>Popular Commissions</h2>
 
-			<h2>Featured Artists</h2>
-			
-			<h2>In Your Area</h2>
-			
-			<h2>Following</h2>
+				<h2>Featured Artists</h2>
+				
+				<h2>In Your Area</h2>
+				
+				<h2>Following</h2>
 
-			<h2>Feed</h2>
+				<h2>Feed</h2>
+			</div>
 		</div>
-	</div>
-</body>
+	</body>
+</html>
--- navbar.php
+++ navbar.php
@@ -6,6 +6,7 @@
     require_once('sessionFunctions.php');
 ?>
 
+<head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <link rel="stylesheet" href="./styles/navbar.css">
@@ -14,15 +15,17 @@
       href="favicon.ico">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
 
-<div id="banner" class="col-lg-12">
-	<a href="./feed.php" class="button">Feed</a>
-	<a href="./commissions_tracker.php" class="button">Commissions Tracker</a>
-	<a href="./users_commissions.php" class="button">My Commissions</a>
-	<a href="./messages.php" class="button">Messages</a>
-	<span id="profile">
-		<a href="./user.php" class="button"><?=$_SESSION['currentUser']['handle']?></a>
-		<img src="./logos_icons/home.png" alt="logo"/> <!--User's profile pic-->
-        <a href="./logout.php" class="button">Log out</a>
-	</span>
-</div>
+<body>
+    <div id="banner" class="col-lg-12">
+        <a href="./feed.php" class="button">Feed</a>
+        <a href="./commissions_tracker.php" class="button">Commissions Tracker</a>
+        <a href="./users_commissions.php" class="button">My Commissions</a>
+        <a href="./messages.php" class="button">Messages</a>
+        <span id="profile">
+            <a href="./user.php" class="button"><?=$_SESSION['currentUser']['handle']?></a>
+            <img src="./logos_icons/home.png" alt="logo"/> <!--User's profile pic-->
+            <a href="./logout.php" class="button">Log out</a>
+        </span>
+    </div>
