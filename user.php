76a9cf93f91321a71b380df4ba41a8caf11c61f9
Stephanie Labastida <stephanielabastida@u.boisestate.edu>
2018-11-07 10:44:39 -0700

    refactored user

--- user.php
+++ user.php
@@ -1,30 +1,14 @@
+
+
+
 <?php
-	$thisPage="HOME";
-	include("navbar.php");
-	require_once("Dao.php");
-	$dao = new Dao();
+    include("navbar.php");
+    require_once("dao.php");
+    $dao = new Dao();
 ?>
-<!DOCTYPE html>
 
-<!-- Initializer stuff -->
-<html lang="en">
-<head>
-<title>Commismatch: User</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link rel="stylesheet" href="./styles/user.css"> 
-  <link rel="icon" 
-      type="image/png" 
-      href="favicon.ico">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-</head>
-<!-- End initializer stuff -->
-
-<body>
 <div class="col-lg-12">
+<link rel="stylesheet" href="./styles/user.css"> 
 			<div class="col-lg-12">
 				<div align="center" id="user" style="position:fixed;" class="col-sm-3">
 					<!-- User profile image, sized to 240x254, with 96 px -->
@@ -67,3 +51,4 @@
 			<h5>Stephanie Labastida. 2018</h5>
 		</div>
 </body>
+</html>
